1f19206d5c9f827a0ca0f5e42099158fd8600cb4
Goonawardana R N W <gtrxone@gmail.com>
2021-11-16 13:58:04 +0530

    final version

--- src/com/gtrxone/Main.java
+++ src/com/gtrxone/Main.java
@@ -3,10 +3,10 @@ package com.gtrxone;
 public class Main {
 
     public static void main(String[] args) {
-        String result = SecondsAndMinutesChallenge.getDurationString(500000,50);
+        String result = SecondsAndMinutesChallenge.getDurationString(65,45);
         System.out.println(result);
 
-        result = SecondsAndMinutesChallenge.getDurationString(4000);
+        result = SecondsAndMinutesChallenge.getDurationString(3000);
         System.out.println(result);
     }
 }
--- src/com/gtrxone/SecondsAndMinutesChallenge.java
+++ src/com/gtrxone/SecondsAndMinutesChallenge.java
@@ -1,21 +1,40 @@
 package com.gtrxone;
 
 public class SecondsAndMinutesChallenge {
+
+    public static final String INVALID_VALUE_MESSAGE = "Invalid value"; // constant
+
     public static String getDurationString(long minutes, long seconds) {
         if (minutes >= 0 && (seconds >= 0 && seconds <= 59)) {
             long hours = minutes / 60;
             long remainingMinutes = minutes % 60;
-            return hours + "h " + remainingMinutes + "m " + seconds + "s";
+
+            String hourString = hours + "h ";
+            if (hours < 10) {
+                hourString = "0" + hourString;
+            }
+
+            String minutesString = remainingMinutes + "m ";
+            if (remainingMinutes < 10) {
+                minutesString = "0" + minutesString;
+            }
+
+            String secondsString = seconds + "s";
+            if (seconds < 10) {
+                secondsString = "0" + secondsString;
+            }
+
+            return hourString + minutesString + secondsString;
         }
-        return "Invalid value";
+        return INVALID_VALUE_MESSAGE;
     }
 
     public static String getDurationString(long seconds) {
         if (seconds > 0) {
             long minutes = seconds / 60;
             long remainingSeconds = seconds % 60;
-            return getDurationString(minutes,remainingSeconds);
+            return getDurationString(minutes, remainingSeconds);
         }
-        return "Invalid value";
+        return INVALID_VALUE_MESSAGE;
     }
 }
